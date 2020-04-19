323f8b54530f0476e317c4b1480c424b7bcd409e
Naveen <chiluka.n@husky.neu.edu>
2020-04-19 12:31:52 -0400

    reverting the mongo URI to the previous config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const {
     PORT = 5000,
     NODE_ENV = 'development',
-    MONGO_URI = 'mongodb://root:passworD1!@ds125912.mlab.com:25912/heroku_mrmtrwnm',
+    MONGO_URI = 'mongodb+srv://ritvik:ritvik123@databaseforbookapi-gzios.mongodb.net/test?retryWrites=true&w=majority',
     SESS_NAME = 'sid',
     SESS_SECRET = 'secret!session',
     SESS_LIFETIME = 1000 * 60 * 60 * 2
